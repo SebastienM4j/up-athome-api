7ce2b1f089af335d1ff3dad00c7129f53db0b733
Benj <benjamin.godard@arcan.fr>
2017-03-24 12:40:36 +0100

    change output dll name

--- Arcan.AtHome.API.Implementation/Library/obj/Debug/netstandard1.2/Library.AssemblyInfo.cs
+++ Arcan.AtHome.API.Implementation/Library/obj/Debug/netstandard1.2/Library.AssemblyInfo.cs
@@ -3,11 +3,11 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Arcan.AtHome.API.Implementation.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Library")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Library")]
+[assembly: System.Reflection.AssemblyProductAttribute("Arcan.AtHome.API.Implementation.Library")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Arcan.AtHome.API.Implementation.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
