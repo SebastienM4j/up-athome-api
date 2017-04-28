2e57071297ec8ab6c21c5952fcca490f09d46a1e
tmarignier <thibault.marignier@gmail.com>
2017-04-28 13:11:19 +0200

    Mise à jour des url des apis

--- Arcan.AtHome.API.Implementation/Library/Client/AtHomeClientFactory.cs
+++ Arcan.AtHome.API.Implementation/Library/Client/AtHomeClientFactory.cs
@@ -4,50 +4,40 @@ namespace Arcan.AtHome.API.Implementation.Queries
 {
     public static class Urls
     {
-        public const string GetCivilite = "api/Administration/Salarie/queries/GetCiviliteQuery";
-        public const string GetFonction = "api/Administration/Salarie/queries/GetFonctionQuery";
-        public const string GetSalariesParIds = "api/Administration/Salarie/queries/GetSalarieParIdQuery";
-        public const string GetSalarieParUtilisateurId = "api/Administration/Salarie/queries/GetSalarieParUtilisateurIdQuery";
-        public const string GetTypesSalaries = "api/Administration/Salarie/queries/GetTypeSalarieQuery";
-        public const string GetVillesParCodePostal = "api/Administration/Ville/queries/GetVilleParCodePostalQuery";
-        public const string GetEtablissementGeographique = "api/Etablissement/Antenne/queries/GetEtablismentGeographiqueQuery";
-        public const string GetAntennes = "api/Etablissement/Antenne/queries/GetAntenneQuery";
-        public const string GetPatientParIds = "api/Patient/Patient/queries/GetPatientParIdsQuery";
-        public const string GetActeParId = "api/Sejour/Sejour/queries/GetActeParIdQuery";
-        public const string GetActes = "api/Sejour/Sejour/queries/GetActesQuery";
-        public const string GetPassagePonctuelParSejourEtDate = "api/Sejour/PassagePonctuel/queries/GetPassagePonctuelParSejourEtDatesQuery";
-        public const string GetPlansSoinsParId = "api/Sejour/PlanSoinSalarie/queries/GetPlansSoinsParIdQuery";
-        public const string GetPlansSoinsParSejoursEtDates = "api/Sejour/PlanSoinSalarie/queries/GetPlansSoinsParSejoursEtDatesQuery";
-        public const string GetSejourParIdExterne = "api/Sejour/Sejour/queries/GetSejourParIdExterneQuery";
-        public const string GetSejoursParDate = "api/Sejour/Sejour/queries/GetSejoursParDateQuery";
-        public const string GetTypesIntervenants = "api/Sejour/Sejour/queries/GetTypesIntervenantsQuery";
-
-        public const string CreerSalarie = "api/Administration/Salarie/commands/CreerSalarieCommand/Execute";
-        
-        
-        public const string AffecterHoraireDeTravailCommand = "api/RH/TimeSlot/commands/AffecterHoraireDeTravailCommand/Execute";
-        public const string DesaffecterHoraireDeTravailCommand = "api/RH/TimeSlot/commands/DesaffecterHoraireDeTravailCommand/Execute";
-        public const string AjouterTypePlageHoraireCommand = "api/RH/TimeSlot/commands/AjouterTypePlageHoraireCommand/Execute";
-        public const string GetTypePlageHoraireParNomQuery = "api/RH/TimeSlot/queries/GetTypePlageHoraireParNomQuery";
-
-
-
-
-        public const string UpdateSalarie = "api/Administration/Salarie/commands/UpdateSalarieCommand/Execute";
-        public const string ManageLinkedIds = "api/Maintenance/LinkedId/commands/ManageLinkedIdsCommand/Execute";
-
-        public const string CreerVille = "api/Administration/Ville/commands/CreateVilleCommand/Execute";
-        public const string CreerAntenne = "api/Etablissement/Antenne/commands/CreerAntenneCommand/Execute";
-
-        public const string CreerActe = "api/Sejour/Acte/commands/CreerActeCommand/Execute";
-        public const string CreerVisiteSalarie = "api/Sejour/VisiteSalarie/commands/CreerVisiteSalarieCommand/Execute";
-        public const string CreerPassagePonctuel = "api/Sejour/PassagePonctuel/commands/CreerPassagePonctuelCommand/Execute";
-        public const string MettreAJourPassagePonctuel = "api/Sejour/PassagePonctuel/commands/MettreAJourPassagePonctuelCommand/Execute";
-        public const string SupprimerPassagePonctuel = "api/Sejour/PassagePonctuel/commands/SupprimerPassagePonctuelCommand/Execute";
-
-        public const string CreerTournee = "api/RH/Tour/commands/CreerTourneeCommand/Execute";
-        public const string AffecteSalarieTournee = "api/RH/Tour/commands/AffecterSalarieATourneeCommand/Execute";
-        public const string AffecterPassageTournee = "api/RH/Tour/commands/AffecterPassageATourneeCommand/Execute";
+        public const string GetCivilite =                       "/api/domain/Salarie/GetCiviliteQuery";
+        public const string GetFonction =                       "/api/domain/Salarie/GetFonctionQuery";
+        public const string GetSalariesParIds =                 "/api/domain/Salarie/GetSalarieParIdQuery";
+        public const string GetSalarieParUtilisateurId =        "/api/domain/Salarie/GetSalarieParUtilisateurIdQuery";
+        public const string GetTypesSalaries =                  "/api/domain/Salarie/GetTypeSalarieQuery";        
+        public const string GetEtablissementGeographique =      "/api/domain/Antenne/GetEtablismentGeographiqueQuery";
+        public const string GetAntennes =                       "/api/domain/Antenne/GetAntenneQuery";
+        public const string GetPatientParIds =                  "/api/domain/Patient/GetPatientParIdsQuery";
+        public const string GetActeParId =                      "/api/domain/Sejour/GetActeParIdQuery";
+        public const string GetActes =                          "/api/domain/Sejour/GetActesQuery";
+        public const string GetPassagePonctuelParSejourEtDate = "/api/domain/PassagePonctuel/GetPassagePonctuelParSejourEtDatesQuery";
+        public const string GetPlansSoinsParId =                "/api/domain/PlanSoinSalarie/GetPlansSoinsParIdQuery";
+        public const string GetPlansSoinsParSejoursEtDates =    "/api/domain/PlanSoinSalarie/GetPlansSoinsParSejoursEtDatesQuery";
+        public const string GetSejourParIdExterne =             "/api/domain/Sejour/GetSejourParIdExterneQuery";
+        public const string GetSejoursParDate =                 "/api/domain/Sejour/GetSejoursParDateQuery";
+        public const string GetTypesIntervenants =              "/api/domain/Sejour/GetTypesIntervenantsQuery";
+        public const string CreerSalarie =                      "/api/domain/Salarie/CreerSalarieCommand/Execute";
+        public const string AffecterHoraireDeTravailCommand =   "/api/domain/TimeSlot/AffecterHoraireDeTravailCommand/Execute";
+        public const string DesaffecterHoraireDeTravailCommand ="/api/domain/TimeSlot/DesaffecterHoraireDeTravailCommand/Execute";
+        public const string AjouterTypePlageHoraireCommand =    "/api/domain/TimeSlot/AjouterTypePlageHoraireCommand/Execute";
+        public const string GetTypePlageHoraireParNomQuery =    "/api/domain/TimeSlot/GetTypePlageHoraireParNomQuery";
+        public const string UpdateSalarie =                     "/api/domain/Salarie/UpdateSalarieCommand/Execute";
+        public const string ManageLinkedIds =                   "/api/domain/LinkedId/ManageLinkedIdsCommand/Execute ";
+        public const string CreerAntenne =                      "/api/domain/Antenne/CreerAntenneCommand/Execute";
+        public const string CreerActe =                         "/api/domain/Acte/CreerActeCommand/Execute";
+        public const string CreerVisiteSalarie =                "/api/domain/VisiteSalarie/CreerVisiteSalarieCommand/Execute";
+        public const string CreerPassagePonctuel =              "/api/domain/PassagePonctuel/CreerPassagePonctuelCommand/Execute ";
+        public const string MettreAJourPassagePonctuel =        "/api/domain/PassagePonctuel/MettreAJourPassagePonctuelCommand/Execute ";
+        public const string SupprimerPassagePonctuel =          "/api/domain/PassagePonctuel/SupprimerPassagePonctuelCommand/Execute";
+        public const string CreerTournee =                      "/api/domain/Tour/CreerTourneeCommand/Execute";
+        public const string AffecteSalarieTournee =             "/api/domain/Tour/AffecterSalarieATourneeCommand/Execute";
+        public const string AffecterPassageTournee =            "/api/domain/Tour/AffecterPassageATourneeCommand/Execute";
+        public const string CreerVille =                        "/api/domain/Ville/CreateVilleCommand/Execute";
+        public const string GetVillesParCodePostal =            "/api/domain/Ville/GetVilleParCodePostalQuery";
     }
 
     public class AtHomeClientFactory
